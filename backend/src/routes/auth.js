56b8aa1e695f70817fe5835748a8598edd1c2ab3
Norinpiseth Ban <63012272+pisethban123@users.noreply.github.com>
2024-11-10 18:14:42 +1100

    Revert "Dev"

--- backend/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from "express"; // Correct ES module import
-import { registerUser, loginUser } from "../controllers/authController.js"; // Ensure correct import
-
-const router = express.Router();
-
-router.post("/register", registerUser); // Route to register a user
-router.get("/login", loginUser); // Route to find a user by username
-
-export default router; // Export the router
